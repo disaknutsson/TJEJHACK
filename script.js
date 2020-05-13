04cb8c972c88d639fc7cbfc83f61db3d58419927
disaknutsson <57992389+disaknutsson@users.noreply.github.com>
2020-05-13 15:27:02 +0200

    fourth upload
    Jag har försökt fixa ett quiz till hemsidan under största delen av lektionen. Jag använde mig av scriptet som fungerade i miniquiz uppgiften men det fungerar inte ändå. Jag vet inte varför. Men alla frågor och knappen syns nu på hemsidan.

---
+++ script.js
@@ -0,0 +1,46 @@
+function check() {
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+
+	var correct = 0;
+
+	if(question1 == "montypython"){
+		correct++;
+	}
+	if(question2 == "turtle"){
+		correct++;
+	}
+	if(question3 == "textp"){
+		correct++;
+
+var meddelanden = ["Du fick tyvärr inga rätt :(", "Du fick ett rätt!", "Du fick två rätt!", "Du fick tre rätt! Bra jobbat!"];
+
+
+var range;
+
+if(correct = 0){
+	range=0;
+}
+
+if(correct = 1){
+	range=1;
+}
+
+if(correct = 2){
+	range=2;
+}
+
+if(correct = 3){
+	range=3;
+}
+	
+	document.getElementById("after_submit").style.visibility="visible";
+
+	document.getElementById("meddelande").innerHTML=meddelanden[range];
+	document.getElementById("number_correct").innerHTML = "Du  fick " + correct + " rätt.";
+	document.getElementById("bilden").src=bilder[range];
+}
+
+
